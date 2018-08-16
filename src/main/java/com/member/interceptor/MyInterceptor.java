4ad8ddc9e84bcc2e4e7823fbbd5be52fe063f622
admin-Simon <admin_simon@icloud.com>
2018-08-16 17:14:00 +0800

    Modified by Simon

---
+++ src/main/java/com/member/interceptor/MyInterceptor.java
@@ -0,0 +1,33 @@
+package com.member.interceptor;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("one");
+        Object userSession = request.getSession().getAttribute("userSession");
+        if (null == userSession) {
+            response.sendRedirect("/out");
+            //返回false 不会进入 two three
+            return false;
+        }
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("two");
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("three");
+        super.afterCompletion(request, response, handler, ex);
+    }
+}
