c8481ae9c7aacd62b3ea0231f33d1cb39e678805
admin-Simon <admin_simon@icloud.com>
2018-08-20 14:59:59 +0800

    Modified By Simon

--- src/main/java/com/lottery/LotteryApplication.java
+++ src/main/java/com/lottery/LotteryApplication.java
@@ -1,9 +1,17 @@
 package com.lottery;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@ComponentScan("com")       //加载@Service @Control注解类
+@MapperScan(value = "com.mapper")  //mybatis 需要扫描mapper接口
+@EnableWebMvc                   //启用mvc
+@EnableTransactionManagement    //启用事务管理
 public class LotteryApplication {
 
     public static void main(String[] args) {
