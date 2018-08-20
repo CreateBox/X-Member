3dabf693b07900528ab66d8da150a37e3aa16e83
admin-Simon <admin_simon@icloud.com>
2018-08-20 14:43:28 +0800

    Modified By Simon

--- src/main/java/com/lottery/control/LotteryControl.java
+++ src/main/java/com/lottery/control/LotteryControl.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LotteryControl {
+    @RequestMapping("/")
+    public String losthost() {
+        return "index";
+    }
+
     @RequestMapping("/{path}")
     public String lottery(@PathVariable("path") String path) {
         return path;
