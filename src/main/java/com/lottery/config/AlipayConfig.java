2a85295d0a8374201d45c7dccfb00a77c1c923d8
Yaxuan86 <18507476506@163.com>
2018-08-23 10:11:53 +0800

    本次修改：     记录了用户最后登录时间     记录了用户登录次数

--- src/main/java/com/lottery/config/AlipayConfig.java
+++ src/main/java/com/lottery/config/AlipayConfig.java
@@ -25,11 +25,11 @@ public class AlipayConfig {
 
     // 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
 //    public static String notify_url = "http://localhost:8080/alipay/notify_url.jsp";
-    public static String notify_url = "http://172.16.7.239:8080/alipay/notify_url.jsp";
+    public static String notify_url = "http://192.168.0.104:8080/alipay/notify_url.jsp";
 
     // 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
 //    public static String return_url = "http://localhost:8080/alipay/return_url.jsp";
-    public static String return_url = "http://172.16.7.239:8080/alipay/return_url.jsp";
+    public static String return_url = "http://192.168.0.104:8080/alipay/return_url.jsp";
 
     // 签名方式
     public static String sign_type = "RSA2";
