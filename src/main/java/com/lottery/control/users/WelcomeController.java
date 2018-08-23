2df529ec72e06071f9d5cf095a9fb82b2bab85ea
admin-Simon <admin_simon@icloud.com>
2018-08-23 09:01:37 +0800

    Modified By Simon

---
+++ src/main/java/com/lottery/control/users/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.lottery.control.users;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+    @RequestMapping("/welcome.html")
+    public String welcome() {
+        return "welcome";
+    }
+}
